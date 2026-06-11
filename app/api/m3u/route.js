162c0b930d591f67b7cbd174c1da9169ca1e7130
ikun <218283296+gdydg@users.noreply.github.com>
2026-06-12 01:07:41 +0800

    Update route.js

--- app/api/m3u/route.js
+++ app/api/m3u/route.js
@@ -11,13 +11,15 @@ export async function GET() {
 
     data.forEach(r => {
         r.streams.forEach((url, idx) => {
-            m3uContent += `#EXTINF:-1 group-title="${r.sid_name}直播", ${r.league} | ${r.title} (线路${idx + 1})\n`;
+            // 统一设置 group-title 为 "皇冠线路"
+            m3uContent += `#EXTINF:-1 group-title="皇冠线路", ${r.sid_name} | ${r.title} (线路${idx + 1})\n`;
             m3uContent += `${url}\n`;
         });
     });
 
     return new NextResponse(m3uContent, {
         headers: {
+            // 强制指定 M3U 文件类型，方便部分播放器直接拉取识别
             'Content-Type': 'application/vnd.apple.mpegurl',
             'Content-Disposition': 'attachment; filename="sports_live.m3u"'
         }
